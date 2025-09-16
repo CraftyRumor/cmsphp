5f7cd37aecc2e462d92024a0cef53f0cfcc79f7f
Carlos Rocha <130331451+CraftyRumor@users.noreply.github.com>
2025-09-16 12:21:21 -0300

    Update scripts.js

--- landing/scripts.js
+++ landing/scripts.js
@@ -3,17 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
-      // Se o elemento estiver visível (intersecting)
+      
       if (entry.isIntersecting) {
-        entry.target.classList.add('animar'); // Adiciona a classe para disparar a animação
-        observer.unobserve(entry.target); // Deixa de observar o elemento após a animação (para não animar de novo)
+        entry.target.classList.add('animar'); 
+        observer.unobserve(entry.target); 
       }
     });
   }, {
-    threshold: 0.1 // Opcional: porcentagem do elemento visível para acionar (10% neste caso)
+    threshold: 0.1
   });
 
-  // Observa cada elemento que queremos animar
+  
   elementosAnimados.forEach(elemento => {
     observer.observe(elemento);
   });
